61f8e44dda27cd0f7b03bd49832bede29a1f8742
mnc13 <masnunnuha@iut-dhaka.edu>
2025-03-29 20:55:04 +0600

    some ui fixes

--- client/src/components/layouts/AuthLayout.jsx
+++ client/src/components/layouts/AuthLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import UI_IMG from "../../assets/img/auth-img.png";
+import UI_IMG from "../../assets/img/image.png";
 
 const AuthLayout = ({ children }) => {
   return <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-lg font-medium text-black">Task Manager</h2>
+        <h2 className="text-6xl font-medium text-black">Task Manager</h2>
         {children}
       </div>
 
-      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
+      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8" style={{ background: 'linear-gradient(to right,rgb(133, 173, 225),#a7f0fd)' }}>
         <img src={UI_IMG} className="w-64 lg:w-[90%]" />
       </div>
     </div>
