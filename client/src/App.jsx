19c129bc5af0882e4bf7666e07d83b6fdcd6dc50
mnc13 <masnunnuha@iut-dhaka.edu>
2025-03-29 20:07:26 +0600

    more fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
-
 import Dashboard from "./pages/Admin/Dashboard";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
@@ -13,14 +12,8 @@ import MyTasks from "./pages/User/MyTasks";
 import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
 import PrivateRoute from "./routes/PrivateRoute";
-
-
-
-
-
-
-
-
+import UserProvider, { UserContext } from "./context/userContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
@@ -47,13 +40,36 @@ const App = () => {
                 path="/user/task-details/:id"
                 element={<ViewTaskDetails />}
               />
-             
-              </Route>  
-            </Routes>
+            </Route>
+
+             {/* Default Route */}
+            <Route path="/" element={<Root />} />
+          </Routes>
         </Router>
       </div>
-      </UserProvider>
-  );
-}         
 
-export default App
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </UserProvider>
+  );
+};
+
+export default App;
+
+const Root = () => {
+  const { user, loading } = useContext(UserContext);
+
+  if(loading) return <Outlet />
+  
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />;
+};
